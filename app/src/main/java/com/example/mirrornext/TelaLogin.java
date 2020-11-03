0a02c49bba5dded7e86a0d762ac32aca079592f5
Lucas Môra <mora.lucas@live.com>
2020-11-03 05:33:44 -0300

    Tela de Esqueci Minha Senha pronta.
    - Adicionados: arquivos de layout, arquivos Java.
    - Atualizado AndroidManifest.xml com a nova atividade.
    - Extraídas algumas strings da Tela de Login que não haviam sido internacionalizadas.

--- app/src/main/java/com/example/mirrornext/TelaLogin.java
+++ app/src/main/java/com/example/mirrornext/TelaLogin.java
@@ -2,13 +2,24 @@ package com.example.mirrornext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TelaLogin extends AppCompatActivity {
+    TextView esqueceu_senha_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        esqueceu_senha_btn = (TextView) findViewById(R.id.esqueceu_senha);
+    }
+
+    public void esqSenha(View v){
+        Intent i = new Intent(this, EsqueciMinhaSenha.class);
+        startActivity(i);
     }
 }
