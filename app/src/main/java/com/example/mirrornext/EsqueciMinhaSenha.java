3307cce1f1519f21d4767021c88455ac898305b2
Lucas Môra <mora.lucas@live.com>
2020-11-05 10:09:05 -0300

    Adicionado código para voltar da tela de Esqueci Senha.

--- app/src/main/java/com/example/mirrornext/EsqueciMinhaSenha.java
+++ app/src/main/java/com/example/mirrornext/EsqueciMinhaSenha.java
@@ -2,13 +2,25 @@ package com.example.mirrornext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EsqueciMinhaSenha extends AppCompatActivity {
+    ImageButton voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esqueci_senha);
+
+        voltar = (ImageButton) findViewById(R.id.voltar_btn);
+    }
+
+    public void voltar(View v) {
+        Intent i = new Intent(this, TelaLogin.class);
+        startActivity(i);
+        finish();
     }
 }
