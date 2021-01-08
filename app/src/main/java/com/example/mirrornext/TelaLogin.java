1167c4893814ddbc18f67a1cc6688f91c39d3b67
Lucas Môra <mora.lucas@live.com>
2021-01-08 20:28:42 -0300

    Resolvido #8
    Adicionado Alerta para confirmação de saída do app.

--- app/src/main/java/com/example/mirrornext/TelaLogin.java
+++ app/src/main/java/com/example/mirrornext/TelaLogin.java
@@ -2,15 +2,19 @@ package com.example.mirrornext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class TelaLogin extends AppCompatActivity {
     TextView esqueceu_senha_btn;
     Button login;
+    ImageButton sair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +23,20 @@ public class TelaLogin extends AppCompatActivity {
 
         esqueceu_senha_btn = (TextView) findViewById(R.id.esqueceu_senha);
         login = (Button) findViewById(R.id.entrar_btn);
+        sair = (ImageButton) findViewById(R.id.sair_btn);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent principal = new Intent(getApplicationContext(), TelaPrincipal.class);
                 startActivity(principal);
+            }
+        });
+
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sair();
             }
         });
     }
@@ -37,5 +49,25 @@ public class TelaLogin extends AppCompatActivity {
     public void atalhos(View v) {
         Intent i = new Intent(this, Atalhos.class);
         startActivity(i);
+    }
+
+    public void sair() {
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.icone_suporte)
+                .setTitle("Fechando aplicativo")
+                .setMessage("Tem certeza que deseja fechar o app?")
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Não", null)
+                .show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        sair();
     }
 }
